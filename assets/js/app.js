1f21e90cf3ade0b141f3d57ef33a07dadc6bf3d3
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-10-16 14:11:34 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,39 @@
-let value = 0;
-let valueRandom = Number((Math.random() * 10).toFixed(0));
-let i = 0;
+// Forma 1 de resolver o problema
 
-console.log(valueRandom);
+// let value = 0;
+// const randomValue = Number(Math.round(Math.random() * 10));
+// let i = 0;
 
-do {
-  i += 1;
+// console.log(randomValue);
 
-  value = Number(
-    prompt("Adivinhe o número no qual estou pensando? Ele está entre 0 e 10.")
-  );
+// do {
+//   i += 1;
 
-  if (value === valueRandom) {
-    alert(`Parabéns! Você adivinhou o número em ${i} tentativas.`);
-    break;
-  } else {
-    alert("Erro! Tente novamente!");
-  }
-} while (value !== valueRandom);
+//   value = Number(
+//     prompt("Adivinhe o número no qual estou pensando? Ele está entre 0 e 10.")
+//   );
+
+//   if (value === randomValue) {
+//     alert(`Parabéns! Você adivinhou o número em ${i} tentativas.`);
+//     break;
+//   } else {
+//     alert("Erro! Tente novamente!");
+//   }
+// } while (value !== randomValue);
+
+// Forma 2 de resolver o problema
+
+let result = prompt(
+  "Adivinhe o número no qual estou pensando? Ele está entre 0 e 10."
+);
+
+const randomNumber = Math.round(Math.random() * 10);
+
+let x = 1;
+
+while (Number(result) !== randomNumber) {
+  result = prompt("Erro! Tente novamente!");
+  x++;
+}
+
+alert(`Parabéns! Você adivinhou em ${x} tentativas.`);
