3c0b495f58fbc71246c2d17993191fb08de90d50
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-10-16 20:14:23 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,8 @@ let option;
 let items = [];
 let i = 0;
 
+// Switch... case
+
 while (option !== 3) {
   option = Number(
     prompt(`Olá! Digite o número da opção desejada:
@@ -11,17 +13,21 @@ while (option !== 3) {
   3. Sair do programa.`)
   );
 
-  if (option === 1) {
-    let item = prompt("Digite o nome do item:");
-    items.push(item);
-  } else if (option === 2) {
-    if (items.length === 0) {
-      alert("Não existe itens cadastrados.");
-    } else {
-      alert(`Sua lista de itens é: ${items.join(", ")}.`);
-    }
-  } else {
-    alert("Tchau tchau! Volte sempre.");
-    break;
+  switch (option) {
+    case 1:
+      let item = prompt("Digite o nome do item:");
+      items.push(item);
+      break;
+
+    case 2:
+      if (items.length === 0) {
+        alert("Não existem itens cadastrados.");
+      } else {
+        alert(`Sua lista de itens é: ${items.join(", ")}.`);
+      }
+      break;
+
+    default:
+      alert("Tchau tchau! Volte sempre.");
   }
 }
