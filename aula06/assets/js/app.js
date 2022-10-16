fa2917a42fa200e301029d5b55643140add64db2
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-10-16 16:13:27 -0300

    Exercises updates

---
+++ aula06/assets/js/app.js
@@ -0,0 +1,47 @@
+// Forma 1 de resolver o problema
+
+// let value = 0;
+// const randomValue = Number(Math.round(Math.random() * 10));
+// let i = 0;
+
+// console.log(randomValue);
+
+// do {
+//   i += 1;
+
+//   value = Number(
+//     prompt("Adivinhe o número no qual estou pensando? Ele está entre 0 e 10.")
+//   );
+
+//   if (value === randomValue) {
+//     alert(`Parabéns! Você adivinhou o número em ${i} tentativas.`);
+//     break;
+//   } else {
+//     alert("Erro! Tente novamente!");
+//   }
+// } while (value !== randomValue);
+
+// Forma 2 de resolver o problema
+
+let result = prompt(
+  "Adivinhe o número no qual estou pensando? Ele está entre 0 e 10."
+);
+
+const randomNumber = Math.round(Math.random() * 10);
+
+let x = 1;
+
+while (Number(result) !== randomNumber) {
+  result = prompt("Erro! Tente novamente!");
+  x++;
+}
+
+if (x == 1) {
+  alert(
+    `Parabéns! O número que pensei foi o ${randomNumber} e você adivinhou em ${x} tentativa.`
+  );
+} else {
+  alert(
+    `Parabéns! O número que pensei foi o ${randomNumber} e você adivinhou em ${x} tentativas.`
+  );
+}
